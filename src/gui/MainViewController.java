87ab153a061541fc86bba9d28a732be5e8e2b177
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-28 15:28:35 -0300

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 	
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -58,6 +59,28 @@ public class MainViewController implements Initializable{
 		mainVbox.getChildren().clear();                                 // AGORA LIMPAMOS TUDO, QUANDO CLICAMOS PRA RECEBER OUTRA TELA
 		mainVbox.getChildren().add(mainMenu);							// ADICIONAMOS O MENU QUE JÁ TINHAMOS MANTIDO NA TELA NOVA -> DEPOIS DE CLICAR
 		mainVbox.getChildren().addAll(newVBox.getChildren()); 			// E ADICIOANMOS OS FILHOS DO ABOUT, PARA APARECER APENAS AS INFORMAÇÕES
+		
+		}
+		catch (IOException e) {                     // TRATATIVA DE ERRO
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	private synchronized void loadView2 (String absoluteName) {   //METODO QUE CARREGA A TELA "syncronized" GARANTE QUE ESTE PROCESSAMENTO NÃO VAI SER INTERROMPIDO DURANTE O MULTITREADH
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVBox = loader.load();
+		
+		Scene mainScene = Main.getMainScene();
+		VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();  // O GET ROOT PEGA A PRIMEIRA LINHA QUE É O SCROLLPANE(CAST) E VBOX (VBOX)
+		
+		Node mainMenu = mainVbox.getChildren().get(0); 					// AQUI PEGAREMOS O VALOR DA PRIMEIRA LINHA DO VBOX, QUE DESEJAMOS MANTER AO CLICAR
+		mainVbox.getChildren().clear();                                 // AGORA LIMPAMOS TUDO, QUANDO CLICAMOS PRA RECEBER OUTRA TELA
+		mainVbox.getChildren().add(mainMenu);							// ADICIONAMOS O MENU QUE JÁ TINHAMOS MANTIDO NA TELA NOVA -> DEPOIS DE CLICAR
+		mainVbox.getChildren().addAll(newVBox.getChildren()); 			// E ADICIOANMOS OS FILHOS DO ABOUT, PARA APARECER APENAS AS INFORMAÇÕES
+		
+		DepartmentListController controller = loader.getController();
+		controller.setDepartmentService(new DepartmentService());
+		controller.updateTableView();
 		
 		}
 		catch (IOException e) {                     // TRATATIVA DE ERRO
