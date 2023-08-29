98d9351acf07a3ec2be4345fa4fa559ec4d98b96
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-29 10:58:56 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,19 +36,22 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 	}
 	
-	private synchronized void loadView (String absoluteName) {   //METODO QUE CARREGA A TELA "syncronized" GARANTE QUE ESTE PROCESSAMENTO NÃO VAI SER INTERROMPIDO DURANTE O MULTITREADH
+	private synchronized <T> void loadView (String absoluteName, Consumer<T> initializingAction) {   //METODO QUE CARREGA A TELA "syncronized" GARANTE QUE ESTE PROCESSAMENTO NÃO VAI SER INTERROMPIDO DURANTE O MULTITREADH
 		try {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 		VBox newVBox = loader.load();
@@ -60,31 +64,13 @@ public class MainViewController implements Initializable{
 		mainVbox.getChildren().add(mainMenu);							// ADICIONAMOS O MENU QUE JÁ TINHAMOS MANTIDO NA TELA NOVA -> DEPOIS DE CLICAR
 		mainVbox.getChildren().addAll(newVBox.getChildren()); 			// E ADICIOANMOS OS FILHOS DO ABOUT, PARA APARECER APENAS AS INFORMAÇÕES
 		
-		}
-		catch (IOException e) {                     // TRATATIVA DE ERRO
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	private synchronized void loadView2 (String absoluteName) {   //METODO QUE CARREGA A TELA "syncronized" GARANTE QUE ESTE PROCESSAMENTO NÃO VAI SER INTERROMPIDO DURANTE O MULTITREADH
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newVBox = loader.load();
-		
-		Scene mainScene = Main.getMainScene();
-		VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();  // O GET ROOT PEGA A PRIMEIRA LINHA QUE É O SCROLLPANE(CAST) E VBOX (VBOX)
-		
-		Node mainMenu = mainVbox.getChildren().get(0); 					// AQUI PEGAREMOS O VALOR DA PRIMEIRA LINHA DO VBOX, QUE DESEJAMOS MANTER AO CLICAR
-		mainVbox.getChildren().clear();                                 // AGORA LIMPAMOS TUDO, QUANDO CLICAMOS PRA RECEBER OUTRA TELA
-		mainVbox.getChildren().add(mainMenu);							// ADICIONAMOS O MENU QUE JÁ TINHAMOS MANTIDO NA TELA NOVA -> DEPOIS DE CLICAR
-		mainVbox.getChildren().addAll(newVBox.getChildren()); 			// E ADICIOANMOS OS FILHOS DO ABOUT, PARA APARECER APENAS AS INFORMAÇÕES
-		
-		DepartmentListController controller = loader.getController();
-		controller.setDepartmentService(new DepartmentService());
-		controller.updateTableView();
+		T controller = loader.getController();
+		initializingAction.accept(controller);
 		
 		}
 		catch (IOException e) {                     // TRATATIVA DE ERRO
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
+
 }
